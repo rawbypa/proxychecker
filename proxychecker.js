65fe474d70796f177903ba9eca7d8418106d8f56
Jas <77278137+pwnjas@users.noreply.github.com>
2022-01-04 14:53:06 +0100

    Update proxychecker.js

--- proxychecker.js
+++ proxychecker.js
@@ -7,24 +7,30 @@ const path = require('path');
 let proxylist = process.argv[2];
 if (!proxylist) {
     console.log("\x1b[31m error\x1b[37m: proxylist missing.");
-    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file>\x1b[0m");
+    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file> <timeout>\x1b[0m");
     process.exit();
 }
 let proxytype = process.argv[3];
 if (!proxytype) {
     console.log("\x1b[31m error\x1b[37m: proxy type missing.");
-    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file>\x1b[0m");
+    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file> <timeout>\x1b[0m");
     process.exit();
 }
 let validproxy = process.argv[4];
 if (!validproxy) {
     console.log("\x1b[31m error\x1b[37m: output file missing.");
-    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file>\x1b[0m");
+    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file> <timeout>\x1b[0m");
     process.exit();
 }
-if (process.argv.length < 4) {
+let timeout = process.argv[5];
+if (!validproxy) {
+    console.log("\x1b[31m error\x1b[37m: output file missing.");
+    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file> <timeout>\x1b[0m");
+    process.exit();
+}
+if (process.argv.length < 5) {
     console.log("\x1b[31m error\x1b[37m: wrong usage!");
-    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file>\x1b[0m");
+    console.log("\x1b[36m usage\x1b[37m: node "+ path.basename(__filename) +" <proxylist to check> <type of proxy> <output file> <timeout>\x1b[0m");
     process.exit(0);
 }
 
@@ -70,7 +76,7 @@ let config = {
     type: proxytype,
     url: 'https://google.com/',
     file: validproxy,
-    timeout: 10000
+    timeout: timeout
 }
   
 let l = require('fs').readFileSync(proxylist, 'utf-8').replace(/\r|\"/gi, '').split("\n")
@@ -80,16 +86,16 @@ l.forEach(a => {
         url: config.url,
         type: config.type,
         proxy: a,
-        timeout: 10000
+        timeout: timeout
     }).then(r => {
         i++ // AllProxyChecked++
         if (r.code !== 200) {
       s++ // BadProxy++
-            console.info(`\x1b[0m</ \x1b[37;101mJASPWN\x1b[0m > [\x1b[91mx\x1b[0m] \x1b[91mBroken proxy \x1b[0m: \x1b[91m${r.proxy.replace("http://", "")} \x1b[0m#${i} (Out checked: \x1b[95m${i}\x1b[0m/\x1b[95m${l.length} \x1b[0m- \x1b[91m${s}\x1b[0m/\x1b[91m${l.length} \x1b[0minvalid proxies)\x1b[0m`)
+            console.info(`[\x1b[91mx\x1b[0m] \x1b[91mBroken proxy \x1b[0m: \x1b[91m${r.proxy.replace("http://", "")} \x1b[0m#${i} (Out checked: \x1b[95m${i}\x1b[0m/\x1b[95m${l.length} \x1b[0m- \x1b[91m${s}\x1b[0m/\x1b[91m${l.length} \x1b[0minvalid proxies)\x1b[0m`)
         } else if (r.code == 200) {
             require('fs').appendFileSync(config.file, a + '\n')
       p++ // GoodProxy++
-      console.info(`\x1b[0m</ \x1b[37;101mJASPWN\x1b[0m > [\x1b[92m!\x1b[0m] \x1b[92mValid  proxy \x1b[0m: \x1b[92m${r.proxy.replace("http://", "")} \x1b[0m#${i} (Out checked: \x1b[95m${i}\x1b[0m/\x1b[95m${l.length} \x1b[0m- \x1b[92m${p}\x1b[0m/\x1b[92m${l.length} \x1b[0mvalid proxies)\x1b[0m`)
+      console.info(`[\x1b[92m!\x1b[0m] \x1b[92mValid  proxy \x1b[0m: \x1b[92m${r.proxy.replace("http://", "")} \x1b[0m#${i} (Out checked: \x1b[95m${i}\x1b[0m/\x1b[95m${l.length} \x1b[0m- \x1b[92m${p}\x1b[0m/\x1b[92m${l.length} \x1b[0mvalid proxies)\x1b[0m`)
         }
     }).catch(e => {
       //return error
